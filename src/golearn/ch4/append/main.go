c048e1d757fa67507727423466b35750e7f9e2b0
liujintong <liujintong@peiwo.cn>
2017-04-12 16:57:17 +0800

    add nonempty

--- src/golearn/ch4/append/main.go
+++ src/golearn/ch4/append/main.go
@@ -2,27 +2,44 @@ package main
 
 import "fmt"
 
-func appendIdx(x []int, y int) []int {
-	var z []int
-	zlen := len(x) + 1
-	if zlen <= cap(x) {
-		z = x[:zlen]
-	} else {
-		zcap := zlen
-		if zcap < 2*len(x) {
-			zcap = 2 * len(x)
-		}
-		z = make([]int, zlen, zcap)
-		copy(z, x)
-	}
-	z[len(x)] = y
-	return z
-}
+// func appendIdx(x []int, y int) []int {
+// 	var z []int
+// 	zlen := len(x) + 1
+// 	if zlen <= cap(x) {
+// 		z = x[:zlen]
+// 	} else {
+// 		zcap := zlen
+// 		if zcap < 2*len(x) {
+// 			zcap = 2 * len(x)
+// 		}
+// 		z = make([]int, zlen, zcap)
+// 		copy(z, x)
+// 	}
+// 	z[len(x)] = y
+// 	return z
+// }
+
+// func appendIdx(x []int, y ...int) []int {
+// 	var z []int
+// 	zlen := len(x) + len(y)
+// 	if zlen <= cap(x) {
+// 		z = x[:zlen]
+// 	} else {
+// 		zcap := zlen
+// 		if zcap < 2*len(x) {
+// 			zcap = 2 * len(x)
+// 		}
+// 		z = make([]int, zlen, zcap)
+// 		copy(z, x)
+// 	}
+// 	copy(z[len(x):], y)
+// 	return z
+// }
 
 func main() {
 	var x, y []int
 	for i := 0; i < 10; i++ {
-		y = appendIdx(x, i)
+		y = appendIdx(x, []int{11, 12})
 		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
 		x = y
 	}

47df63b3b5fb4387002d945b3f16546810456fcc
liujintong <liujintong@peiwo.cn>
2017-04-12 19:05:15 +0800

    add dedup charcount in ch4

--- src/golearn/ch4/test/main.go
+++ src/golearn/ch4/test/main.go
@@ -26,17 +26,64 @@ import "fmt"
 // 	}
 // 	fmt.Println(r[0:3])
 // }
+//
+// func main() {
+// 	months := [...]string{1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
+// 	Q2 := months[4:7]
+// 	summer := months[6:9]
+// 	fmt.Println(Q2)
+// 	fmt.Println(summer)
+//
+// 	var runes []rune
+// 	for _, r := range "Hello, 世界" {
+// 		runes = append(runes, r)
+// 	}
+// 	fmt.Printf("%q\n", runes)
+// }
+
+// func main() {
+// 	ages := make(map[string]int)
+// 	fmt.Printf("%q\n", ages)
+// 	ages = map[string]int{
+// 		"alice":   11,
+// 		"charlie": 12,
+// 	}
+// 	for key, value := range ages {
+// 		fmt.Printf("%s:%d\n", key, value)
+// 	}
+// }
+
+func equal(x, y map[string]int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || yv != xv {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-	months := [...]string{1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
-	Q2 := months[4:7]
-	summer := months[6:9]
-	fmt.Println(Q2)
-	fmt.Println(summer)
-
-	var runes []rune
-	for _, r := range "Hello, 世界" {
-		runes = append(runes, r)
+	map1 := map[string]int{
+		"a": 1,
+		"b": 2,
 	}
-	fmt.Printf("%q\n", runes)
+	map2 := map[string]int{
+		"a": 1,
+		"b": 2,
+	}
+	map3 := map[string]int{
+		"a": 1,
+		"b": 0,
+	}
+	map4 := map[string]int{
+		"a": 1,
+		"c": 0,
+	}
+	fmt.Println(equal(map1, map2))
+	fmt.Println(equal(map2, map3))
+	fmt.Println(equal(map3, map4))
 }

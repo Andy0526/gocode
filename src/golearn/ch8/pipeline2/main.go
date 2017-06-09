01140c5176b8ef6569d83beaf2cbf6ef473b6fab
liujintong <liujintong@peiwo.cn>
2017-06-09 16:32:04 +0800

    fix

--- src/golearn/ch8/pipeline2/main.go
+++ src/golearn/ch8/pipeline2/main.go
@@ -6,21 +6,19 @@ func main() {
 	naturals := make(chan int)
 	squares := make(chan int)
 	go func() {
-		for x := 0; x < 10; x++ {
+		for x := 0; x < 100; x++ {
 			naturals <- x
 		}
+		close(naturals)
 	}()
-
 	go func() {
-		for {
-			x, ok := <-naturals
-			if !ok {
-				break
-			}
+		for x := range naturals {
 			squares <- x * x
 		}
+		close(squares)
 	}()
-	for {
-		fmt.Println(<-squares)
+
+	for x := range squares {
+		fmt.Println(x)
 	}
 }

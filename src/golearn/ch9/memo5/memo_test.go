17b261bf683a3292ae113afa184ef3f6b7bce9f3
lewsan <532235905@qq.com>
2017-06-18 22:56:32 +0800

    week

---
+++ src/golearn/ch9/memo5/memo_test.go
@@ -0,0 +1,21 @@
+package memo_test
+
+import (
+	"golearn/ch9/memo5"
+	"golearn/ch9/memotest"
+	"testing"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Concurrent(t, m)
+}
